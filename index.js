c93a47d82fe77ed7e297afb24235a4414bcc3d71
burakekincioglu <burakekincioglu93@gmail.com>
2022-01-14 11:11:29 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ */
+ import React from 'react';
+import {AppRegistry} from 'react-native';
+//import App from './App';
+import {name as appName} from './app.json';
+import Header from './src/components/Header';
+
+
+const App = () => (
+    <Header headerText={'Ornek Proje'}/>
+);
+
+
+//export default App; // App uygulamasının ucu nerde? en son çağırılma yeri neresi ??
+AppRegistry.registerComponent(appName, () => App);
