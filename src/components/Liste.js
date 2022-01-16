bc31b4fa15ca325544563a36b174fd77e42ab92c
burakekincioglu <burakekincioglu93@gmail.com>
2022-01-16 15:30:09 +0300

    first request tryed

--- src/components/Liste.js
+++ src/components/Liste.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import axios from 'axios'
 
 
-const Liste = () => {
+//servislere bağlanabilmek için class haline getireceğiz
+class Liste extends Component {
+    componentWillMount() {
+        console.log('componentWillMount');
+        axios.get('https://reqres.in/api/users')
+        .then(response => console.log(response));
+    }
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
 
+    render() {
+        console.log('render');
+        return (
+            <View style={{marginTop: 5}}>
+            <Text> Example Project List</Text>
+            </View>
+        );
+    }
 }
+
+export default Liste;
