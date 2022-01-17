6b9ae0648aa473fb769111f03f98528283d408e9
burakekincioglu <burakekincioglu93@gmail.com>
2022-01-17 11:27:21 +0300

    detay component styles added

--- src/components/Detay.js
+++ src/components/Detay.js
@@ -2,9 +2,60 @@ import React from 'react';
 import {Text, View} from 'react-native';
 
 const Detay = (props) => {
+    const {ContainerStyle, subContainerStyle} = styles;
     return (
+        <View style={ContainerStyle}>
+        
+        <View style={subContainerStyle}>
         <Text>{props.data.last_name}</Text>
+        </View>
+
+        <View style={subContainerStyle}>
+        <Text>{props.data.last_name}</Text>
+        </View>
+
+        <View style={subContainerStyle}>
+        <Text>{props.data.last_name}</Text>
+        </View>
+
+        </View>
     );
 };
 
+const styles = {
+
+    containerStyle: {
+      borderWidth: 1,
+      borderRadius: 2,
+      borderColor: '#ddd',
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.1,
+      shadowRadius: 2,
+      elevation: 1,
+      marginLeft: 5,
+      marginRight: 5,
+      marginTop: 10,
+  
+    },
+    subContainerStyle: {
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative'
+  },
+  ImageStyle: {
+    height: 300,
+    flex: 1
+  
+  },
+  titleStyle: {
+    fontSize: 18
+  }
+  
+  };
+  
 export default Detay;
